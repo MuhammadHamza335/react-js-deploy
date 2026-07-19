c0e429e01a0682649871c6cbf4c2b8444324111c
mhamzadevoirrc <m.hamza@oirrc.net>
2026-07-19 18:19:47 +0500

    changed text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          AOA Deploy
+          AOA AOA AOA Deploy
         </a>
       </header>
     </div>
